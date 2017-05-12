9976f05b91d943f72e62a3cfe7deba0c1510f5df
Diana Fisher <diana.fisher@gmail.com>
2017-05-12 12:53:34 -0700

    Add endpoint to reverse a string.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = { name: 'Wes', age: 100, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(wes);
+
+  // get the data from the query string
+  // res.send(req.query.name);
+  res.send(req.query);
+});
+
+// make a new endpoint to take in a name and reverse it
+router.get('/reverse/:name', (req, res) => {
+  // res.send('it works!');
+  // res.send(req.params.name);
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
