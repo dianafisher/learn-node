af30e56a31a7fbf29dc8fd79b0bda352d08a9f93
Diana Fisher <diana.fisher@gmail.com>
2017-05-12 20:57:27 -0700

    Set up a route-specific middleware.

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
 app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
 
+// app.use indicates global middleware
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Form submits will be url encoded via the bodyParser package
 // Takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
+// import our controller
+const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', (req, res) => {
-  const wes = { name: 'Wes', age: 100, cool: true };
-  // res.send('Hey! It works!');
-  // res.json(wes);
+// here we are using route-specific middleware
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
-  // get the data from the query string
-  // res.send(req.query.name);
-  res.send(req.query);
-});
+// router.get('/', (req, res) => {
+//   const wes = { name: 'Wes', age: 100, cool: true };
+//   // res.send('Hey! It works!');
+//   // res.json(wes);
+//
+//   // get the data from the query string
+//   // res.send(req.query.name);
+//   // res.send(req.query);
+//
+//   // render out a template
+//   res.render('hello', {
+//     name: 'wes',
+//     dog: req.query.dog,
+//     title: 'I love food'
+//   });
+// });
 
 // make a new endpoint to take in a name and reverse it
 router.get('/reverse/:name', (req, res) => {
