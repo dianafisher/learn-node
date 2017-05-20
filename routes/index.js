288e727414fa77c846ebe0b54fd1cd574cb601e6
Diana Fisher <diana.fisher@gmail.com>
2017-05-19 21:02:03 -0700

    Create login page.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 // import our controller
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');  // object destructuring
 
 // here we are using route-specific middleware
@@ -51,5 +52,7 @@ router.get('/reverse/:name', (req, res) => {
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
 
 module.exports = router;
